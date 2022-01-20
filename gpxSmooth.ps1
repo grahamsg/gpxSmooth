fc5843ad7ede0de579cad41ede1b621a26a1b600
grahamsg <grahampg@gmail.com>
2022-01-19 21:43:45 -0800

    Updated gpxSmooth.ps1 so that it runs as a script instead of loading a function

--- gpxSmooth.ps1
+++ gpxSmooth.ps1
@@ -1,15 +1,12 @@
-function Smooth-GPX { 
-    param (
-        $file
-    )
+param (
+    $file
+)
 
-	$datetimeRegex = "(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z";
+$datetimeRegex = "(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z";
 
-	Get-Content $file | ForEach-Object { 
-		if ($_ -match $datetimeRegEx) {
-			echo $_;
-			break;
-		}
+Get-Content $file | ForEach-Object { 
+	if ($_ -match $datetimeRegEx) {
+		echo $_;
+		break;
 	}
-
 }
