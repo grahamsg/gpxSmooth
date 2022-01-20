2649ff998ed3719a455f0b166bf0a2a8ba061a0c
Graham <grahampg@gmail.com>
2022-01-19 21:48:35 -0800

    Update gpxSmooth.ps1
    Changed name of $file to $inFile, since I'll later want to write output to a new file.

--- gpxSmooth.ps1
+++ gpxSmooth.ps1
@@ -1,10 +1,10 @@
 param (
-    $file
+    $inFile
 )
 
 $datetimeRegex = "(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z";
 
-Get-Content $file | ForEach-Object { 
+Get-Content $inFile | ForEach-Object { 
 	if ($_ -match $datetimeRegEx) {
 		echo $_;
 		break;
