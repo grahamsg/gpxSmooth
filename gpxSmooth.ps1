3b832068f3b52d9aa8d17e304849211e18288425
grahamsg <grahampg@gmail.com>
2022-01-20 08:48:17 -0800

    committing current changes in case my computer crashes or needs a reboot

--- gpxSmooth.ps1
+++ gpxSmooth.ps1
@@ -1,8 +1,11 @@
 param (
-    $inFile
+    $inFile, $outFile
 )
 
 $datetimeRegex = "(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z";
+$activityStartTime;
+$removeStartOffset;
+$removeEndOffset;
 
 Get-Content $inFile | ForEach-Object { 
 	if ($_ -match $datetimeRegEx) {
