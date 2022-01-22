0d6b941d9b261b84c013b57290dbcbdda31f0601
grahamsg <grahampg@gmail.com>
2022-01-21 20:16:13 -0800

    filling out more of gpxSmooth

--- gpxSmooth.ps1
+++ gpxSmooth.ps1
@@ -2,14 +2,40 @@ param (
     $inFile, $outFile
 )
 
-$datetimeRegex = "(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z";
+$datetimeRegex = "(?<date>\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z)";
 $activityStartTime;
-$removeStartOffset;
-$removeEndOffset;
+
+# prompt the user for the start and end timespans
+
+
+# for now I'll just set them
+$removeStartOffset = New-TimeSpan -Minutes 5 -Seconds 27; # The first point that should be removed in days, hours, minutes, seconds after $activityStartTime
+$removeEndOffset   = New-TimeSpan -Minutes 5 -Seconds 49; # The last point that should be removed in days, hours, minutes, seconds after $activityStartTime
+$inRemove = "before"; # Status flag with values "before", "remove", and "after"
+
+
 
 Get-Content $inFile | ForEach-Object { 
-	if ($_ -match $datetimeRegEx) {
-		echo $_;
-		break;
+    $curLine = $_;
+	if ((-not $activityStartTime) -and $curLine -match $datetimeRegEx) {
+		$activityStartTime = Get-Date $matches['date'];
+		"The activity started at $activityStartTime";
+        $removeStartAbsolute = $activityStartTime.Add($removeStartOffset);
+        $removeEndAbsolute   = $activityStartTime.Add($removeEndOffset);
+        "Will remove $removeStartAbsolute through $removeEndAbsolute";
 	}
+    switch ($inRemove) {
+        "before" {
+            if (($curLine -match $datetimeRegEx) -and ((Get-Date $matches['date'] -ge $removeStartAbsolute))) {
+                "$curLine: I was in the before time, now moving to remove time";
+                $inRemove = "remove";
+            }
+        }
+        "remove" {
+            
+        }
+        "after"  {
+            
+        }
+    }
 }
